f5beb459fc4cb330c9ff43d16a895a03e7972307
athulya2000 <athulyav00@gmail.com>
2022-12-06 15:57:01 +0530

    last commit

--- src/main/java/com/example/courseapp_backend/controller/CourseController.java
+++ src/main/java/com/example/courseapp_backend/controller/CourseController.java
@@ -1,13 +1,17 @@
 package com.example.courseapp_backend.controller;
 
+import com.example.courseapp_backend.dao.CourseDao;
 import com.example.courseapp_backend.model.Course;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CourseController {
+    @Autowired
+    private CourseDao dao;
+    @CrossOrigin(origins = "*")
     @PostMapping(path="/add",consumes = "application/json",produces = "application/json")
     public String Add(@RequestBody Course c){
         System.out.println(c.getCoursetitle().toString());
@@ -15,10 +19,12 @@ public class CourseController {
         System.out.println(c.getCoursevenu().toString());
         System.out.println(c.getDuration().toString());
         System.out.println(c.getDate().toString());
+        dao.save(c);
         return "course added successfully";
     }
+    @CrossOrigin(origins = "*")
     @GetMapping("/viewall")
-    public String View(){
-        return "Welcome to course view all page";
+    public List<Course> View(){
+        return (List<Course>) dao.findAll();
     }
 }
